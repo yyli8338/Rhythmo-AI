91dafc8b9cab4b1c217b1fd845b2cb07ad6df573
Antoine-Lee <antoinekllee@gmail.com>
2024-03-16 19:48:54 +0800

    Add all interview question data object

--- App/client/pages/InterviewScreen.js
+++ App/client/pages/InterviewScreen.js
@@ -10,6 +10,92 @@ import TransparentStyledButton from "../components/TransparentStyledButton";
 import StyledButton from '../components/StyledButton';
 import DropdownComponent from '../components/DropdownComponent';
 
+const questionData = [
+    {
+        questionNum: 1,
+        questionText: "Which body part is injured?",
+        type: "dropdown",
+        options: ["Ankle", "Knee", "Shin", "Foot", "Thigh", "Hand", "Wrist", "Shoulder", "Hip", "Back", "Groin", "Neck", "Elbow", "Chest", "Head", "Face"]
+    },
+    {
+        questionNum: 2,
+        questionText: "What does the pain feel like?",
+        type: "dropdown",
+        options: ["Sore", "Stabbing", "Aching", "Burning", "Catching", "Tearing", "Grinding", "Throbbing", "Sharp", "Dull", "Numb", "Tingling", "Pins and Needles"]
+    },
+    {
+        questionNum: 3,
+        questionText: "How often do you feel the pain?",
+        type: "dropdown",
+        options: ["Consistently", "Every 1-2 hours", "Every 1-2 days", "Every 1 week", "Greater than 1 week intervals"]
+    },
+    {
+        questionNum: 4,
+        questionText: "Is there a particular time of day the pain is more intense?",
+        type: "dropdownWithOther",
+        options: ["Yes", "No"],
+        otherText: "Yes", // if "Yes" is selected, show text input
+        otherPrompt: "When is the pain more intense OR what triggers the pain?"
+    },
+    {
+        questionNum: 5,
+        questionText: "For how long does the pain bother you?",
+        type: "dropdown",
+        options: ["Around 5-10 minutes", "Around 30 minutes", "Around 1-2 hours", "Greater than 2 hours", "Constantly"]
+    },
+    {
+        questionNum: 6,
+        questionText: "On a scale of 1 to 10, how bad does the injury hurt?",
+        type: "slider",
+        // for slider, option text will be shown when the number will selected
+        // numbers on slider will be 1 to length of options
+        options: ["Pain is barely noticeable and is often forgotten about", "Pain is minor, though there are occasional moments of stronger pain", "Pain is noticeable and can be distracting, but not disruptive", "Pain is moderate and can affect athletic activities", "Pain is very distracting, but can be managed to continue to participate in some activities", "Pain is starting to intefere with daily activities and may result in difficutly concentrating", "Pain is severe, significantly limits daily life, and can affect sleep", "Pain is intense and physical activity is very limited", "Pain is excruciating, physical activity is impossible, and nearly every movment hurts", "Pain is unspeakable. Likely bedridden and delirious. This is a rare case."]
+    },
+    {
+        questionNum: 7,
+        questionText: "What are the symptoms you're experiencing? (Select all that apply)",
+        type: "multiSelectWithOther",
+        options: ["Sudden, severe pain", "Swelling or bruising", "Not being able to place weight on a leg, knee, ankle, or foot", "A bone or joint is visibly out of place", "Weakness of an injured limb", "Not being able to move a joint normally", "Other"],
+        otherText: "Other", // if "Other" is selected, show text input
+        otherPrompt: "What other symptoms are you experiencing? (include all that apply)"
+    },
+    {
+        questionNum: 8,
+        questionText: "Are there any movements here that you can't do?",
+        type: "multiSelectWithOther",
+        options: ["Bending", "Twisting", "Lifting", "Carrying", "Sleeping", "Jumping", "Stair climbing", "Sit to stand", "Rolling over", "Walking", "Squatting", "Lunging", "Kicking", "Other"],
+        otherText: "Other", // if "Other" is selected, show text input
+        otherPrompt: "What other movements can't you do?"
+    },
+    {
+        questionNum: 9,
+        questionText: "Did you hear any sound such as a tear, rip, snap, or pop?",
+        type: "dropdownWithOther",
+        options: ["Yes", "No"],
+        otherText: "Yes", // if "Yes" is selected, show text input
+        otherPrompt: "What sound did you hear?"
+    },
+    {
+        questionNum: 10,
+        questionText: "Are you experiencing any tingling or numbness anywhere in your body?",
+        type: "dropdownWithOther",
+        options: ["Yes", "No"],
+        otherText: "Yes", // if "Yes" is selected, show text input
+        otherPrompt: "Where are you experiencing the tingling or numbness?"
+    },
+    {
+        questionNum: 11,
+        questionText: "Does it just hurt to touch?",
+        type: "dropdown",
+        options: ["Yes", "No"]
+    },
+    {
+        questionNum: 12,
+        questionText: "Please describe the situation in which the injury was caused AND/OR when the pain started. Include any relevant details. More information will help us diagnose your injury with greater accuracy.",
+        type: "textInput"
+    },
+]
+
 const data = [
     { label: "Ankle", value: "Ankle" },
     { label: "Knee", value: "Knee" },
